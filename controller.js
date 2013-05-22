5d1a5d98e4f69a7006ff258187e0cde4c39caca0
oncletom <thomas@oncle-tom.net>
2013-05-22 22:15:55 +0200

    Calling directly the spreadsheet in HTTPS

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@
 function TalkController($scope){
   $scope.talks = [];
 
-  $.ss("http://spreadsheets.google.com/tq?key=" + TalkController.getUrlArgument('key'))
+  $.ss("https://spreadsheets.google.com/tq?key=" + TalkController.getUrlArgument('key'))
     .setQuery("select *")
     .setField("date,first_name,blah,last_name,talk_40,themes,audience,audience_level,keywords,language,title,description,sex,location,email,phone,transportation,hosting,expectations,freespeech,talk_20,talk_lt,talk_workshop,url,rating,remarks,total")
     .send(function(){
