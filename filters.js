31f066151e452b11da25b4976131b48cc9db9a14
oncletom <thomas@oncle-tom.net>
2013-05-22 22:16:06 +0200

    Moodify filter

--- filters.js
+++ filters.js
@@ -6,4 +6,28 @@ angular.module('talksFilters', [])
         .replace(/(-1|Non|No)/i, "label label-empty")
         .replace(/(0|Envisageable|Maybe)/i, "label label-maybe");
     };
+  })
+  .filter('moodify', function(){
+    return function(rating){
+      var label = [];
+
+      if (rating < 0){
+        label.push('negative');
+      }
+
+      if (rating === 0){
+        label.push('neutral');
+      }
+
+      if (rating > 0){
+        label.push('positive');
+      }
+
+      if (rating === 2)
+      {
+        label.push('favorite');
+      }
+
+      return label.join(' ');
+    };
   });
