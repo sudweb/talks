42aa076761e5bee753f39f42e4026c482e8e7d71
oncletom <thomas@oncle-tom.net>
2013-05-22 18:51:38 +0200

    Displaying stuff with Angular

---
+++ controller.js
@@ -0,0 +1,24 @@
+function TalkController($scope){
+  $scope.talks = [];
+
+  $.ss("http://spreadsheets.google.com/tq?key=" + TalkController.getUrlArgument('key'))
+    .setQuery("select *")
+    .setField("date,first_name,blah,last_name,talk_40,themes,audience,audience_level,keywords,language,title,description,sex,location,email,phone,transportation,hosting,expectations,freespeech,talk_20,talk_lt,talk_workshop,url,rating,remarks,total")
+    .send(function(){
+      $scope.talks = this;
+
+      $scope.$apply('');
+    });
+}
+
+TalkController.getUrlArgument = function getUrlArgument(key){
+  var value = '';
+
+  location.search.replace(new RegExp(key+'=([^&$]+)'), function(m, matched_value){
+    value = matched_value;
+  });
+
+  return value;
+}
+
+TalkController.$inject = ['$scope'];
