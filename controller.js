d72cc2711fa9f28cad6d487405e0fda4b12538cf
oncletom <thomas@oncle-tom.net>
2013-05-23 11:54:27 +0200

    Column mapping

--- controller.js
+++ controller.js
@@ -42,47 +42,66 @@ TalkController.getUrlArgument = function getUrlArgument(key){
  * @returns {Array.<Object>}
  */
 TalkController.mapResponseFields = function mapResponseFields(response){
+  var fields = TalkController.mapResponseHeaderFields(response.table.cols);
+
   return response.table.rows.map(function(row){
     var data = {};
 
     row.c.forEach(function(column, index){
-      data[ TalkController.fields[index] ] = column.v;
+      if (fields[index]){
+        data[ fields[index] ] = column.v;
+      }
     });
 
     return data;
   });
 };
 
-//@todo handle mapping with field names to avoid relying on column order (good for BC)
-TalkController.fields = [
-  "date",
-  "first_name",
-  "blah",
-  "last_name",
-  "talk_40",
-  "themes",
-  "audience",
-  "audience_level",
-  "keywords",
-  "language",
-  "title",
-  "description",
-  "sex",
-  "location",
-  "email",
-  "phone",
-  "transportation",
-  "hosting",
-  "expectations",
-  "freespeech",
-  "talk_20",
-  "talk_lt",
-  "talk_workshop",
-  "url",
-  "rating",
-  "remarks",
-  "total"
-];
+/**
+ * Create a data mapping between response column labels and rows index
+ *
+ * @param {Array.<{v: String, label: String}>} columns
+ * @returns {Object} Key/Value array-like object
+ */
+TalkController.mapResponseHeaderFields = function mapResponseHeaderFields(columns){
+  var fields = {};
+  var mapping = TalkController.fieldMapping;
+
+  columns.forEach(function(column, index){
+    if (mapping[column.label]){
+      fields[index] = mapping[column.label];
+    }
+  });
+
+  return fields;
+};
+
+TalkController.fieldMapping = {
+  "TS": "first_name",
+  "Nom": "last_name",
+  "Thématique concernée": "themes",
+  "Public ciblé": "audience",
+  "Niveau d'expérience souhaité du public": "audience_level",
+  "Mots clés caractérisant la conférence": "keywords",
+  "Langue parlée pendant la conférence": "language",
+  "Titre de la conférence": "title",
+  "Description": "description",
+  "Sexe": "genre",
+  "Provenance": "location",
+  "Adresse email": "email",
+  "Numéro de téléphone": "phone",
+  "Mode de déplacement envisagé": "transportation",
+  "Besoin d'être hébergé": "hosting",
+  "Souhaits, attentes et remarques vis à vis de Sud Web": "expectations",
+  "Autres remarques et questions.": "freespeech",
+  "Format(s) d'intervention possible pour ce sujet [Conférence de 20 minutes]": "talk_20",
+  "Format(s) d'intervention possible pour ce sujet [Lightning Talk de 5 minutes]": "talk_lt",
+  "Format(s) d'intervention possible pour ce sujet [Atelier/Dojo/BarCamp]": "talk_workshop",
+  "URL": "url",
+  "N/A": "rating",
+  "Remarques": "remarks",
+  "Note": "total"
+};
 
 // Explicit injection
 TalkController.$inject = ['$scope', '$http'];
